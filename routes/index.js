2a303a0c8b142a27f9d2693985453d13db3eed12
anasmullappally <mohammedanas362@gmail.com>
2022-09-19 23:33:04 +0530

    small design

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('../Controller/controller')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let sup = [{a:'1',b:2},{a:'1',b:2},{a:'1',b:2}]
+  res.render('index', { title: 'Express',supplies:sup });
 });
 
 module.exports = router;
